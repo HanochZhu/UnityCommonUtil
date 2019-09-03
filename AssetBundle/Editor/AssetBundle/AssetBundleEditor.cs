1e7680f33209224860739e314f453bf430b7e403
ThisskyMain <943264652@qq.com>
2019-09-04 00:25:29 +0800

    the first version of assetbundle tools

---
+++ AssetBundle/Editor/AssetBundle/AssetBundleEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+
+public class AssetBundleEditor
+{
+    [MenuItem("MyTool/AssetBundle/CreatAllAssetBundle")]
+    public static void creatAllAssetBundle()
+    {
+
+        string outputPath = Application.streamingAssetsPath + "/Window/AB/";
+
+        if (!Directory.Exists(outputPath))
+        {
+            Directory.CreateDirectory(outputPath);
+        }
+
+        AssetBundleManifest abm =  BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath + "/Window/AB/", BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows);
+    }
+}
---
+++ AssetBundle/Script/AssetBundleManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetBundleManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadAssetBundleAsync(Application.streamingAssetsPath + "/Window/AB/newquad.asset"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadAssetBundle(string path)
+    {
+
+    }
+
+    public IEnumerator LoadAssetBundleAsync(string path)
+    {
+        //manifest of face
+
+        AssetBundleCreateRequest abcr = AssetBundle.LoadFromFileAsync(path);
+
+        // why yield
+        yield return abcr;
+
+        if (abcr.isDone)
+        {
+            AssetBundle ab = abcr.assetBundle;
+            string[] names = ab.GetAllAssetNames();
+
+            foreach (var name in names)
+            {
+                AssetBundleRequest abr = ab.LoadAllAssetsAsync();
+
+                yield return abr;
+
+                Object[] assets = abr.allAssets;
+
+                foreach (var item in assets)
+                {
+                    GameObject o = GameObject.Instantiate<GameObject>(item as GameObject);
+                }
+            }
+        }
+
+    }
+}
