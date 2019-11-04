b01e63109e6084d059be96c739014ccc3de00286
HanochZhu <943264652@qq.com>
2019-11-04 11:27:55 +0800

    commit a ui utils

---
+++ UI/UIComponentHelper.cs
@@ -0,0 +1,44 @@
+﻿/**
+ * UI 辅助类
+ * 提供UI相关的查找、等等功能的函数
+ * 与具体UI无关
+ * 通过string来查找
+ * 
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIComponentHelper
+{
+    private Transform transform;
+    public Transform ComponentTransform
+    {
+        get
+        {
+            return transform;
+        }
+        private set
+        {
+            transform = value;
+        }
+    }
+    // index使用方法
+    // Transform t = UIComponentHelper[parent1][parent2]..[curent].ComponentTransform
+    public UIComponentHelper this[string name]
+    {
+        get
+        {
+            return null;
+        }
+        set
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public void SetRootAnchor(Transform transform)
+    {
+        this.ComponentTransform = transform;
+    }
+}
